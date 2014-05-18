1b2623d2fbb576c7a7e68783b303bb5b0c803585
juliha <bla@bla.com>
2014-05-18 10:32:00 +0200

    finished Block generator and integrated into block world

--- src/main/java/BlockGenerator.java
+++ src/main/java/BlockGenerator.java
@@ -10,13 +10,14 @@ public class BlockGenerator {
 
     public static final int[][] quad = {{1,1}, {1,1}};
     public static final int[][] rect = {{1,1,1,1}};
+    public static final int[][] rect2 = {{1},{1},{1},{1}};
     public static final int[][] lShape1 = {{1,0,0},{1 ,1, 1}};
     public static final int[][] lShape2 = {{0,0,1},{1 ,1, 1}};
     public static final int[][] tShape = {{0,1,0},{1 ,1, 1}};
     public static final int[][] zShape1 = {{1,1,0},{0 ,1, 1}};
     public static final int[][] zShape2 = {{0,1,1},{1 ,1, 0}};
 
-    public static final List<int[][]> possibleShapes =ImmutableList.of(quad, rect, lShape1, lShape2, tShape, zShape1, zShape2);
+    public static final List<int[][]> possibleShapes =ImmutableList.of(quad, rect, lShape1, rect2, lShape2, tShape, zShape1, zShape2);
 
     public static int[][] getRandomShape() {
         double high = possibleShapes.size()-1;
