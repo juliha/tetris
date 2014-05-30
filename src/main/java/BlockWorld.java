c0d320f371e3207d3e9938f5d6b2625d09f266f0
juliha <bla@bla.com>
2014-05-30 10:39:37 +0200

    deleted obsolete run method

--- src/main/java/BlockWorld.java
+++ src/main/java/BlockWorld.java
@@ -197,23 +197,7 @@ public class BlockWorld extends JPanel implements Runnable {
         }
     }
 
-    private void createAndShowGUI() {
-        JFrame frame = new JFrame("Bloxx");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(this);
-        frame.pack();
-        frame.setVisible(true);
-    }
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                BlockWorld game = new BlockWorld(10, 17, 20);
-                game.createAndShowGUI();
-            }
-        });
-    }
 
 
 }
