8f6809b171a9c20e870b5444876473210f658f76
juliha <bla@bla.com>
2014-05-30 09:31:41 +0200

    deleted println

--- src/main/java/BlockGenerator.java
+++ src/main/java/BlockGenerator.java
@@ -11,7 +11,6 @@ public class BlockGenerator {
     public static AbstractBlock getRandomShape() {
         double high = 7;
         int i = (int) (Math.random() * (high));
-        System.out.println(i);
         switch(i) {
             case 0 :
                 return new LShape(4,0);
